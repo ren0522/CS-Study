6ddd201842d459244ac202d094ce609bf6b64e8b
ren0522 <ren_@kakao.com>
2022-06-09 17:01:40 +0900

    테스트

---
+++ Test/1/1_04.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._1
+{
+    internal class _1_04
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int result;
+
+            result = num++;
+            Console.WriteLine("result : " + result);
+
+            result = ++num;
+            Console.WriteLine("result : " + result);
+
+            result = num--;
+            Console.WriteLine("result : " + result);
+
+            result = --num;
+            Console.WriteLine("result : " + result);
+
+        }
+    }
+}
