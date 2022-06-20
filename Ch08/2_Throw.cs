6893a575f78d76cc9451cec9a2703f01fe5c7053
ren0522 <ren_@kakao.com>
2022-06-20 11:48:57 +0900

    CH08 수업예제

---
+++ Ch08/2_Throw.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch08
+{
+    class Box
+    {
+        private int width;
+        private int height;
+
+        public int Width { 
+            get { return width; } 
+            set 
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+                else
+                {   //가로길이가 0보다 작을 경우 예외를 강제 발생
+                    throw new Exception("너비는 0보다 커야 합니다.");
+                }
+            }
+        }
+        public int Height { 
+            get { return height; } 
+            set 
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {   //세로 길이가 0보다 작을 경우 예외를 강제 발생
+                    throw new Exception("높이는 0보다 커야 합니다.");
+                }
+            }
+        }
+        public Box(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public void Area()
+        {
+            Console.WriteLine("Box 넓이 : "+ width * height);
+        }
+    }
+    internal class _2_Throw
+    {
+        static void Main1(string[] args)
+        {
+            try
+            {   //강제 발생된 예외를 처리
+                Box box1 = new Box(10, -10);
+                box1.Area();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
